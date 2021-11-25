ec42b53e27839460db988eae7b5b02e880ca8495
Sylvie-B <unsolo@hotmail.fr>
2021-11-25 15:29:20 +0100

    work on math

---
+++ exo.js
@@ -0,0 +1,42 @@
+let res1 = Math.floor(34.589456);
+console.log("1 : " + res1);
+
+let res2 = Math.floor(68.412784);
+console.log("2 : " + res2);
+
+let res3 = Math.ceil(39.456);
+console.log("3 : " + res3);
+
+let res4 = Math.ceil(27.34876541)
+console.log("4 : " + res4);
+
+let res5 = Math.round(5.68);
+console.log("5 : " + res5);
+
+let res6 = Math.round(5.41);
+console.log("6 : " + res6);
+
+let res7 = Math.trunc(6.12457);
+console.log("7 : " + res7);
+
+let res8 = Math.trunc(9.568124);
+console.log("8 : " + res8);
+
+let res9 = Math.min(5, 1, 12, -1, 45);
+console.log("9 : " +res9);
+
+let res10 = Math.min(9, 12, 16, 44, 3);
+console.log("10 : " +res10);
+
+let res11 = Math.max(5, 1, 12, -1, 45);
+console.log("11 : " + res11);
+
+let res12 = Math.max(9, 12, 16, 44, 3);
+console.log("12 : " + res12);
+
+let res13 = Math.abs(-6);
+console.log("13 : " + res13);
+
+let res14 = Math.abs(-56);
+console.log("14 : " + res14);
+
